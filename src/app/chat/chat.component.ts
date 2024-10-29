9e19dd6049c527e9fc5064abff0eb04c8e3c2e4b
Jorgemrqz <estebamarquez2702@gmail.com>
2024-10-29 17:33:38 -0500

    agregando chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  socket: any;
+  messages: string[] = [];
+  message: string = '';
 
+  ngOnInit() {
+    // Conectar al servidor WebSocket
+    this.socket = io('http://localhost:3000');
+    
+    // Recibir mensajes
+    this.socket.on('message', (msg: string) => {
+      this.messages.push(msg);
+    });
+  }
+
+  sendMessage() {
+    if (this.message.trim()) {
+      // Enviar mensaje al servidor
+      this.socket.emit('message', this.message);
+      this.message = '';
+    }
+  }
 }
